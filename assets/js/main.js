11dd46b8617dc7aab4310f80a5a4d0f6f04e56d8
Myllena Tayná <myllenatayna21@gmail.com>
2022-11-25 15:48:46 -0300

    Utilizado a função map para diminuir a verbosidade do codigo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
 
 
-const offset = 0
-const limit = 10 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -24,14 +20,8 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeapi.getPokemons().then((pokemons) => {
-    const listItems =  []
-
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon))
-    }
-
-    console.log(listItems)
+pokeapi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    
 })
     
