8a89da30c17cae98503a5a10bbf727e913b02fca
Myllena Tayná <myllenatayna21@gmail.com>
2022-11-27 11:22:47 -0300

    convertido o modelo do PokeApi para nosso modelo.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,9 +2,26 @@
 
 const pokeapi = {}
 
+function convertPokeAoiDetailtoPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    
+    const types =pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeapi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
             .then((response) => response.json())
+            .then(convertPokeAoiDetailtoPokemon)
 }
 
 pokeapi.getPokemons = (offset = 0, limit = 5 ) => {
